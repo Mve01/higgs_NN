c5d5350bf9f70ab4ba7f3ff4c938cacad43ec752
Melle van Eldijk <mveldijk@stbc-i1.nikhef.nl>
2025-09-15 11:57:01 +0200

    added data_loader

---
+++ data_loaders.py
@@ -0,0 +1,21 @@
+import torch
+from drellyan import DrellYanDataset
+
+
+def get_data(dataset: str):
+    dataset = dataset.lower()
+    if dataset == "drellyan":
+        return DrellYanDataset()
+
+    raise ValueError(f"Unknown dataset '{dataset}'. Please choose 'drellyan'.")
+
+
+def get_data_loaders(data, batch_size: int = 128):
+    train = torch.from_numpy(data.x_train)
+    val = torch.from_numpy(data.x_val)
+    test = torch.from_numpy(data.x_test)
+
+    train_loader = torch.utils.data.DataLoader(train, batch_size=batch_size, shuffle = True)
+    val_loader = torch.utils.data.DataLoader(val, batch_size=batch_size)
+    test_loader = torch.utils.data.DataLoader(test, batch_size=batch_size)
+    return train_loader, val_loader, test_loader
